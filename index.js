95df6c501f4946225eed295ede8cf3a41757f3e1
omarkarios <markariosoketch@gmail.com>
2025-09-09 06:17:44 +0300

    Completed lab.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = "Grace Hopper";
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
